6170a6d98dfa5880c29b14b823e006cfbf9cd813
Xavier Pouyat <xpouyat@microsoft.com>
2024-06-04 19:50:04 +0200

    Update

--- SampleNet8.0/SimpleEncodingAndPublishing.cs
+++ SampleNet8.0/SimpleEncodingAndPublishing.cs
@@ -46,24 +46,25 @@ namespace Sample
             // MK.IO Client creation
             var client = new MKIOClient(config["MKIOSubscriptionName"]!, config["MKIOToken"]!);
 
-            // Create a new input Asset and upload the specified local video file into it.
+            // Create a new input Asset and upload the specified local video file into it. We use the delete flag to delete the blob and container if we delete the asset in MK.IO
             _ = await CreateInputAssetAsync(client, config["StorageName"], config["TenantId"], inputAssetName, InputMP4FileName);
 
-            // Output from the encoding Job must be written to an Asset, so let's create one
+            // Output from the encoding Job must be written to an Asset, so let's create one. We use the delete flag to delete the blob and container if we delete the asset in MK.IO
             _ = await CreateOutputAssetAsync(client, config["StorageName"], outputAssetName, $"encoded asset from {inputAssetName} using {TransformName} transform");
 
             // Ensure that you have the desired encoding Transform. This is really a one time setup operation.
             _ = await CreateOrUpdateTransformAsync(client, TransformName);
 
+            // Submit a joib request to MK.IO to apply the specified Transform to a given input video.
             _ = await SubmitJobAsync(client, TransformName, jobName, inputAssetName, outputAssetName, InputMP4FileName);
 
+            // Polls the status of the job and wait for it to finish.
             var encodingJob = await WaitForJobToFinishAsync(client, TransformName, jobName);
-
-            // Encoding is finished.
 
             if (encodingJob.Properties.State != JobState.Finished)
             {
                 Console.WriteLine("Encoding job cancelled or in error.");
+                await CleanIfUserAcceptsAsync(client, inputAssetName, outputAssetName, TransformName, jobName);
                 return;
             }
 
@@ -82,10 +83,10 @@ namespace Sample
             // Display streaming paths and test player urls
             await ListStreamingUrlsAsync(client, locatorName);
 
+            await CleanIfUserAcceptsAsync(client, inputAssetName, outputAssetName, TransformName, jobName, locatorName);
             Console.WriteLine("Press Enter to close the app.");
-            Console.ReadLine();
         }
-
+        
         /// <summary>
         /// Creates a new input Asset and uploads the specified local video file into it.
         /// </summary>
@@ -103,7 +104,7 @@ namespace Sample
                 null,
                 storageName!,
                 $"input asset {fileToUpload}",
-                AssetContainerDeletionPolicyType.Retain,
+                AssetContainerDeletionPolicyType.Delete,
                 null,
                 new Dictionary<string, string> { { "typeAsset", "source" } }
             );
@@ -144,7 +145,8 @@ namespace Sample
                 null,
                 storageName!,
                 description,
-                AssetContainerDeletionPolicyType.Retain, null,
+                AssetContainerDeletionPolicyType.Delete,
+                null,
                 new Dictionary<string, string> { { "typeAsset", "encoded" } }
                 );
             Console.WriteLine($"Output asset '{assetName}' created.");
@@ -294,17 +296,83 @@ namespace Sample
         /// <returns></returns>
         private static async Task<JobSchema> WaitForJobToFinishAsync(MKIOClient client, string transformName, string jobName)
         {
+            const int SleepIntervalMs = 10 * 1000;
             JobSchema job;
 
             do
             {
+                await Task.Delay(SleepIntervalMs);
                 job = await client.Jobs.GetAsync(transformName, jobName);
                 Console.WriteLine(job.Properties.State + (job.Properties.Outputs.First().Progress != null ? $" {job.Properties.Outputs.First().Progress}%" : string.Empty));
-                Thread.Sleep(10000);
             }
             while (job.Properties.State == JobState.Queued || job.Properties.State == JobState.Scheduled || job.Properties.State == JobState.Processing);
 
             return job;
+        }
+
+        /// <summary>
+        /// Clean the created resources if user accepts
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="inputAssetName"></param>
+        /// <param name="outputAssetName"></param>
+        /// <param name="transformName"></param>
+        /// <param name="jobName"></param>
+        /// <param name="locatorName"></param>
+        /// <returns></returns>
+        private static async Task CleanIfUserAcceptsAsync(MKIOClient client, string inputAssetName, string outputAssetName, string transformName, string jobName, string locatorName = null)
+        {
+            Console.WriteLine("Do you want to clean the created resources (assets, job, etc) ? (Y/N)");
+            var response = Console.ReadLine();
+            if (response == "Y" || response == "y")
+            {
+                try
+                {
+                    await client.Assets.DeleteAsync(inputAssetName);
+                }
+                catch
+                {
+                    Console.WriteLine($"Error deleting asset '{inputAssetName}'.");
+                }
+
+                try
+                {
+                    await client.Assets.DeleteAsync(outputAssetName);
+                }
+                catch
+                {
+                    Console.WriteLine($"Error deleting asset '{outputAssetName}'.");
+                }
+
+                try
+                {
+                    await client.Jobs.DeleteAsync(transformName, jobName);
+                }
+                catch
+                {
+                    Console.WriteLine($"Error deleting asset '{outputAssetName}'.");
+                }
+
+                try
+                {
+                    await client.Transforms.DeleteAsync(transformName);
+                }
+                catch
+                {
+                    Console.WriteLine($"Error deleting transform '{transformName}'.");
+                }
+
+                if (locatorName != null)
+                    try
+                    {
+                        await client.StreamingLocators.DeleteAsync(locatorName);
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"Error deleting locator '{locatorName}'.");
+                    }
+                Console.WriteLine("Cleaning done.");
+            }
         }
     }
 }
